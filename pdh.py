752f3dd31c125840018b5ec66ae7b5f3f922b2db
Karin Kruuse <karin.kruuse@ut.ee>
2024-10-01 01:34:04 +0200

    try to compare to aprox

--- pdh.py
+++ pdh.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 from collections.abc import Iterable
 
+np.seterr(divide='ignore')
+
 p = 2*np.pi
 
 wavelength = 1064*10**-9
@@ -12,38 +14,41 @@ f = c/wavelength
 
 dT = 1/f/4.2
 print("Sample spacing", dT)
-t = np.arange(0, 100*p/f, dT)
+t = np.arange(0, 1500*p/f, dT)
 N = len(t)
 print(N, "samples")
 
-div = np.random.uniform(1.1, 7)
-fLO = f/div
-print("laser frequency", np.round(f*10**-12, 1), "THz")
-print("modulating frequency", np.round(fLO*10**-12, 1), "THz")
-LO = 0.4*np.sin(p*fLO*t)
 
-noise_switch = True
-if noise_switch:
-    noise_L = np.random.normal(0, 0.2, N)
-    L_mod = np.sin(p*f*t + LO + noise_L)
-else:
-    L_mod = np.sin(p*f*t + LO)
-
-"""cavity"""   
 R = 0.99
+r = np.sqrt(R)
 T = 1 - R
 
 M = 4*R/T**2
 n = 1
 m = 1
 L = 2 * wavelength/n*m
-
 const = 2*n*L
-F = np.pi*np.sqrt(R)/T
-#print("Finesse", F)
+fsr = c/2/L
 
-# Returns the intensity
+fLO = f/3
+print("laser frequency", np.round(f*10**-12, 1), "THz")
+print("modulating frequency", np.round(fLO*10**-9, 1), "GHz")
+LO = 0.3*np.sin(p*fLO*t)
+
+noise_switch = True
+if noise_switch:
+    noise_L = np.random.normal(0, 0.6, N)
+    L_mod = np.sin(p*f*t + LO + noise_L)
+else:
+    L_mod = np.sin(p*f*t + LO)
+
+
+
+# Returns the intensity that is transmitted
 def cavity(f, plot=False):
+    F = np.pi*np.sqrt(R)/T
+    print("Finesse", F)
+
     _wavelength = c/f
     _phi = p*n*L/_wavelength
     _I = 1/(1+M*np.sin(_phi)**2)
@@ -66,13 +71,16 @@ def cavity(f, plot=False):
     return _I
  
 
+# the reflection for E
+def reflection_coef(f):
+    ex = np.exp(-1j*const*p/c*f)
+    return -r*(ex - 1)/(1-R*ex)
+
 show_laser = True
 if(show_laser):
-    fig, (ax2, ax3, ax1) = plt.subplots(3, 1, figsize=(12, 8))
+    fig, (ax2, ax1, ax3) = plt.subplots(3, 1, figsize=(12, 8))
 
-
-    
-    yf = fft(L_mod)[:N//2]
+    yf = 2.0/N *fft(L_mod)[:N//2]
     xf = fftfreq(N, dT)[:N//2]
 
     lower_bound = (f - fLO) * 0.8
@@ -82,27 +90,26 @@ if(show_laser):
     cropped_xf = xf[slice_indices]
     cropped_yf = yf[slice_indices]
 
-    ax2.plot(cropped_xf, 2.0/N * np.abs(cropped_yf))
-    #print(slice_indices)
-    #ax2.vlines(xf[slice_indices[0][0]], 0, 1, color="red")
-    #ax2.vlines(xf[slice_indices[0][-1]], 0, 1, color="red")
-    #ax2.vlines(f, 0, 1, color="black", linestyle="--")
-    #ax2.vlines(f+fLO, 0, 1, color="black", linestyle="--")
-    #ax2.vlines(f-fLO, 0, 1, color="black", linestyle="--")
+    ax2.plot(xf, np.real(yf))
+    ax2.plot(xf, np.imag(yf), color='gray', linestyle="--")
     ax2.set_xlabel("Frequency (Hz)")
     ax2.grid()
 
-    PD_I = np.multiply(1 - cavity(cropped_xf), cropped_yf)
-    ax3.plot(cropped_xf, cavity(cropped_xf))
-    f2wl = lambda x : c/x 
-    wl2f = lambda x : c/x
-    secax = ax3.secondary_xaxis('top', functions=(f2wl, wl2f))
-    secax.set_xlabel('wavelength [nm]')
-    ax3.grid()
-
-    ax1.plot(cropped_xf, PD_I)
+    temp = reflection_coef(xf) * yf
+    PD_I = np.real(temp)**2 + np.imag(temp)**2
+    ax1.plot(xf, PD_I)
     ax1.grid()
 
+    #reflected = 1-cavity(xf)
+    #PD_I = reflected * yf
+    #L_mod_reconstructed = ifft(PD_I)
+    #ax3.plot(t[:N//2], L_mod_reconstructed)
+    #f2wl = lambda x : c/x * 10**9
+    #wl2f = lambda x : c/x * 10**-9
+    #secax = ax3.secondary_xaxis('top', functions=(f2wl, wl2f))
+    #secax.set_xlabel('wavelength [nm]')
+    ax3.grid()
 
     plt.tight_layout()
     plt.show()
+
