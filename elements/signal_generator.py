34062e6ca2148257c9534b26e4f879e0b749c7a6
Karin Kruuse <karin.kruuse@aei.mpg.de>
2025-01-15 15:50:09 +0100

    better noise

--- elements/signal_generator.py
+++ elements/signal_generator.py
@@ -5,52 +5,100 @@ from scipy.constants import c
 
 class LaserSignal:
 
-    def __init__(self, wavelength=1064e-9, nr_cyc=500000, f_mod=2.4*10**9, mod_depth=0.15, noise_std=0.1, 
-                 noise_amplitude=15, modulation_type='analog'):
+
+    def __init__(self, wavelength=1064e-9, nr_cyc=500000, dT=0):
 
         self.wavelength = wavelength
         self.frequency = c / wavelength
-        self.dT = 1 / self.frequency / 2.5
+        if dT == 0: self.dT = 1 / self.frequency / 2.5
+        else: self.dT = dT
         self.t = np.arange(0, nr_cyc * 2 * np.pi / self.frequency, self.dT)
-        self.noise_std = noise_std
-        self.noise_amplitude = noise_amplitude
-        self.f_mod = f_mod
-        self.mod_depth = mod_depth
-        self.modulation_type = modulation_type
         self.laser = None
+        self.laser_noise = None
+        self.clock_jitter = None
         self.N = len(self.t)
+        print("Number of samples:", self.N)
 
-    def generate_signal(self):
-        # Generate modulation signal based on type
-        if self.modulation_type == 'analog':
-            LO = self.mod_depth * np.sin(2 * np.pi * self.f_mod * self.t)
-        elif self.modulation_type == 'digital':
-            LO = self.mod_depth * np.sign(np.sin(2 * np.pi * self.f_mod * self.t))  # Square wave
+    @classmethod
+    def from_duration(cls, wavelength=1064e-9, duration=1.0):
+        """
+        Alternative constructor to initialize the LaserSignal object with the length of the data stream in seconds.
+        """
+        frequency = c / wavelength
+        nr_cyc = int(duration * frequency / (2 * np.pi))
+        print("laser wavelegth: ", wavelength)
+        print(f"Number of cycles: {nr_cyc}")
+        return cls(wavelength, nr_cyc)
+    
+    @classmethod
+    def from_duration_and_N(cls, N, wavelength=1064e-9, duration=1.0):
+        """
+        Alternative constructor to initialize the LaserSignal object with the length of the data stream in seconds.
+        """
+        frequency = c / wavelength
+        dT = duration / N
+        nr_cyc = duration * frequency / (2 * np.pi)
+        print("laser wavelegth: ", wavelength)
+        print(f"Number of cycles: {nr_cyc}")
+        return cls(wavelength, nr_cyc, dT)
+
+    def generate_signal(self, mod_depth=0.15, f_mod=2.4*10**9, laser_noise_amplitude=0.001, laser_noise_std=0.1, clock_noise_amplitude=0.1, clock_noise_std=0.01, modulation_type='analog'):
         
-        noise = self.noise_amplitude * np.random.normal(0, self.noise_std, len(self.t))
-        self.laser = np.exp(1j * (2 * np.pi * self.frequency * self.t + LO + noise))
+        """
+        if modulation_type == 'analog':
+            sideband = mod_depth * np.sin(2 * np.pi * f_mod * self.t)
+        elif modulation_type == 'digital':
+            sideband = mod_depth * np.sign(np.sin(2 * np.pi * f_mod * self.t))  # Square wave
+        """
+        
+        self.f_mod = f_mod
+
+        # Clock noise
+        # GAUSSIAN-ish
+        self.clock_jitter = np.cumsum(clock_noise_amplitude * np.random.normal(0, clock_noise_std, len(self.t)))
+        sideband = mod_depth * np.sin(2 * np.pi * f_mod * self.t + self.clock_jitter)
+
+        # Add GAUSSIAN noise to the signal
+        # GAUUSIAN-ish
+        self.laser_noise = np.cumsum(laser_noise_amplitude * np.random.normal(0, laser_noise_std, len(self.t)))
+        self.laser = np.exp(1j * (2 * np.pi * self.frequency * self.t + sideband + self.laser_noise))
     
     def get_signal(self):
+        """
+        Get the generated laser signal and time array.
+        
+        Returns:
+        tuple: (laser signal, time array)
+        """
         return self.laser, self.t
 
+    def add_noise():
+        """
+        Generate your desired power spectral density in the frequency domain (for example Gaussian, 1/f, etc).
+        Randomize phase.
+        FFT back to the time domain.
+        """
+        #the spectrum doesnt really matter that much? 
+        return
+
     def calculate_fft(self):
+        """
+        Calculate the FFT of the laser signal.
+        
+        Returns:
+        tuple: (frequencies, FFT values)
+        """
         fft_values = fft(self.laser) * 2 / self.N
         freqs = fftfreq(len(self.t), self.dT)
         return freqs, fft_values
 
-    def write_to_file(self, filename):
-        freqs, fft_values = self.calculate_fft()
-        with open(filename, 'w') as f:
-            # Write parameters as a comment
-            f.write(f"# wavelength={self.wavelength}, nr_cyc={len(self.t)}, f_mod={self.f_mod}, "
-                    f"mod_depth={self.mod_depth}, noise_std={self.noise_std}, "
-                    f"noise_amplitude={self.noise_amplitude}, modulation_type={self.modulation_type}\n")
-            # Write time, signal, and FFT to the file
-            for i in range(len(self.t)):
-                f.write(f"{self.t[i]:.6e}, {self.laser[i].real:.6e}, {self.laser[i].imag:.6e}, "
-                        f"{freqs[i]:.6e}, {fft_values[i].real:.6e}, {fft_values[i].imag:.6e}\n")
-    
     def plot_spectrum(self, lim_on=20):
+        """
+        Plot the spectrum of the laser signal.
+        
+        Parameters:
+        lim_on (int): Limit for the x-axis around the modulation frequency.
+        """
         L_mod_fft = fft(self.laser)[1:self.N//2] * 2 / self.N
         freqs = fftfreq(len(self.t), self.dT)[1:self.N//2]
         
@@ -66,13 +114,18 @@ class LaserSignal:
         plt.show()
 
     def plot_modified_spectrum(self, mod_signal):
+        """
+        Plot the spectrum of a modified signal.
+        
+        Parameters:
+        mod_signal (array): The modified signal to plot.
+        """
         L_mod_fft = fft(mod_signal)[1:self.N//2] * 2 / self.N
         freqs = fftfreq(len(self.t), self.dT)[1:self.N//2]
         
         plt.figure(figsize=(10, 6))
         plt.plot(freqs, np.abs(L_mod_fft), color="r")
 
-        #plt.xlim(self.frequency - 20*self.f_mod, self.frequency + 20*self.f_mod)
         plt.xlim(0, 150*10**6)
 
         plt.title("Spectrum of the Laser Signal")
@@ -81,11 +134,34 @@ class LaserSignal:
         plt.grid()
         plt.show()
 
+    def write_to_file(self, filename):
+        """
+        Write the laser signal and its FFT to a file.
+        
+        Parameters:
+        filename (str): Name of the file to write to.
+        """
+        freqs, fft_values = self.calculate_fft()
+        with open(filename, 'w') as f:
+            # Write parameters as a comment
+            f.write(f"# wavelength={self.wavelength}, nr_cyc={len(self.t)}, f_mod={self.f_mod}, "
+                    f"mod_depth={self.mod_depth}, noise_std={self.noise_std}, "
+                    f"noise_amplitude={self.noise_amplitude}, modulation_type={self.modulation_type}\n")
+            # Write time, signal, and FFT to the file
+            for i in range(len(self.t)):
+                f.write(f"{self.t[i]:.6e}, {self.laser[i].real:.6e}, {self.laser[i].imag:.6e}, "
+                        f"{freqs[i]:.6e}, {fft_values[i].real:.6e}, {fft_values[i].imag:.6e}\n")
     
     @classmethod
     def read_from_file(cls, filename):
         """
         Read the signal and parameters from a file and recreate a LaserSignal object.
+        
+        Parameters:
+        filename (str): Name of the file to read from.
+        
+        Returns:
+        LaserSignal: A new LaserSignal object with the data from the file.
         """
         with open(filename, 'r') as f:
             # Read the parameters from the first line
@@ -117,6 +193,3 @@ class LaserSignal:
             obj.laser = laser_signal
             obj.N = len(t)
             return obj
-
-
-    
--- lisa-toy-model.py
+++ lisa-toy-model.py
@@ -5,20 +5,41 @@ from scipy.constants import c
 
 from elements.signal_generator import LaserSignal
 
-laser1 = LaserSignal()
-laser1.generate_signal()
-#laser.plot_spectrum()
-laser_signal1, t1 = laser1.get_signal()
+wl = lambda f: c / f
 
 
-laser2 = LaserSignal()
-laser2.generate_signal()
-#laser.plot_spectrum()
-laser_signal2, t2 = laser2.get_signal()
 
-freqs2, fft_values2 = laser2.calculate_fft()
+# f1 has to be bigger here
+f1 = 15 * 10**6 # MHz order
+wl1 = wl(f1)
 
-delay = 0
+f_mod1 = 10 * 10**3
 
-freqs2_del = 0
-# Time-Shifting Property of Fourier Transform
+laser1 = LaserSignal.from_duration(wavelength=wl1, duration=1)
+laser1.generate_signal(f_mod=f_mod1)
+laser1.plot_spectrum()
+
+N = laser1.N
+f2 = 10 * 10**6 # MHz order
+wl2 = wl(f2)
+f_mod2 = 10 * 10**3
+
+laser2 = LaserSignal.from_duration_and_N(N=N, wavelength=wl2, duration=1)
+laser2.generate_signal(f_mod=f_mod2)
+laser2.plot_spectrum()
+
+
+
+
+"""
+1) Generate like reasonable USO frequencies, but at like maybe on the order of kHz?
+2) Add that to the lasers
+3) The lasers have a way lower frequency, like 10s MHz maybe
+
+then
+4) delay one signal
+5) PD
+6) ADC -- HOW????
+"""
+
+
