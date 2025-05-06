a957d096bdc261659edd8216afcdad0cb424a1b3
Karin Kruuse <karin.kruuse@aei.mpg.de>
2025-05-06 11:19:08 +0200

    moving esitulsed

--- pdh stuff/pdh.py
+++ pdh stuff/pdh.py
@@ -11,32 +11,33 @@ wavelength = 1064e-9
 f = c / wavelength
 
 # Time domain
-dT = 1 / f / 3.3
-print("Sample spacing", dT)
-t = np.arange(0, 300000 * p / f, dT)
+dT = 1 / f / 2.4
+print(f"Sample spacing {np.round(dT*1e-15, 3)} fs")
+signal_length = 5000000 # random fking units
+t = np.arange(0, signal_length * p / f, dT)
 N = len(t)
 print(N, "samples")
 
 # Mirror reflectivity and cavity parameters
-R = 0.995 # NOTICE THIS IS CAPITALIZED
+R = 0.9995 # NOTICE THIS IS CAPITALIZED
 r = np.sqrt(R)
 T = 1 - R
 
 # Geometry and cavity definition
 n = 1
-m = 5000
-L = 2 * wavelength / n * m
+m = 1
+L = 1e5 * wavelength # wl on minigi 1e-6 m
 print("L is", L, "m")
 const = 2 * n * L
 fsr = c / (2 * L)
-print("FSR is", np.round(fsr * 1e-12, 3), "THz")
+print("FSR is", np.round(fsr * 1e-6, 3), "MHz")
 
 # Modulation
-fLO = 0.04 * fsr
+fLO = 0.1 * fsr
 mod_depth = 0.15
 LO = mod_depth * np.sin(p * fLO * t)
 print("Laser frequency", np.round(f * 1e-12, 1), "THz")
-print("Modulation frequency", np.round(fLO * 1e-9, 1), "GHz")
+print("Modulation frequency", np.round(fLO * 1e-6, 1), "MHz")
 
 # Cavity finesse
 finesse = np.pi * np.sqrt(R) / (1 - R)
@@ -48,23 +49,32 @@ def reflection_coef(f):
     return -r * (ex - 1) / (1 - R * ex)
 
 # Frequency sweep for error signal (coarse)
-n_points_error = 1000
-delta_f = 0.1 * fsr
+n_points_error = 50
+delta_f = 1.1 * fLO
 fs_error = np.linspace(f - delta_f, f + delta_f, n_points_error)
+print("Frequency range for error signal (THz)", np.round((fs_error[0])*1e-12, 6), np.round((fs_error[-1])*1e-12, 6))
+print("step size for the error signal", (fs_error[1] - fs_error[0])*1e-6, "MHz (this should be smaller than the mod frequency)")
 
-# Frequency sweep for reflectance/transmittance (fine)
-n_points_fine = 10000
-n_fsr = 3
-delta_f_fine = n_fsr * fsr
-fs_fine = np.linspace(f - delta_f_fine, f + delta_f_fine, n_points_fine)
+
 
 # Time-frequency variables
 xf = fftfreq(N, dT)
+print("FFT frequency resolution is", np.round((xf[1] - xf[0]) * 1e-6, 3), "MHz (This has to be smaller than the modulating frequency)")
 error_signal = []
 
+L_mod = np.exp(1j * (p * f * t + LO))#L_mod = np.exp(1j * (p * f_i * t + LO + noise_L))
+E0 = fft(L_mod)[1:N//2] * 2 / N
+freqs = xf[1:N//2]
+
+plt.figure(figsize=(10, 6))
+plt.plot(freqs, np.abs(E0), color="r")
+plt.xlim(f - 2*fLO, f + 2*fLO)
+plt.show()
+
 # Loop for error signal only (coarse)
-for f_i in fs_error:
-    print(f_i)
+for i in range(n_points_error):
+    f_i = fs_error[i]
+    print("On iteration", i+1, "of", n_points_error)
     #noise_L = np.random.normal(0, 0.5, N)
     L_mod = np.exp(1j * (p * f_i * t + LO))#L_mod = np.exp(1j * (p * f_i * t + LO + noise_L))
     E0 = fft(L_mod)
@@ -75,31 +85,39 @@ for f_i in fs_error:
     mixed_spectrum = fft(mixed_signal)
     error_signal.append(mixed_spectrum[0])
 
+"""    
+# Frequency sweep for reflectance/transmittance (fine)
+n_points_fine = 10000
+n_fsr = 3
+delta_f_fine = n_fsr * fsr
+fs_fine = np.linspace(f - delta_f_fine, f + delta_f_fine, n_points_fine)
 # Reflectance and transmittance (fine resolution)
 refl_coef_vals = reflection_coef(fs_fine)
 reflectance = np.abs(refl_coef_vals)**2
 transmittance = 1 - reflectance
-
+"""
 # Plotting
-fig, axs = plt.subplots(2, 1, figsize=(8, 6), sharex=False)
+#fig, axs = plt.subplots(1, 1, figsize=(8, 6), sharex=False)
 
 # --- Error signal and Re{r(f)} ---
-axs[0].plot((fs_error - f) / fsr, np.real(reflection_coef(fs_error)), color="black", label="Re[Reflection Coefficient]")
-axs[0].set_ylabel("Re[r(f)]")
-ax2 = axs[0].twinx()
-ax2.plot((fs_error - f) / fsr, np.real(error_signal), color="navy", label="PDH Error Signal")
+plt.plot((fs_error - f) *1e-6, np.real(reflection_coef(fs_error)), color="black", label="Re[Reflection Coefficient]")
+plt.ylabel("Re[r(f)]")
+ax2 = plt.twinx()
+ax2.plot((fs_error - f) *1e-6, np.real(error_signal), color="navy", label="PDH Error Signal")
 ax2.set_ylabel("Error Signal", color="navy")
 ax2.tick_params(axis='y', labelcolor="navy")
-axs[0].grid()
-axs[0].set_title(f"PDH Error Signal and Reflection Coefficient\nm = {m}, Modulation frequency = {np.round(fLO * 1e-9, 1)} GHz")
+plt.grid()
+plt.title(f"PDH Error Signal and Reflection Coefficient\nm = {m}, Modulation frequency = {np.round(fLO * 1e-6, 3)} MHz")
+plt.xlabel("Frequency around Carrier [MHz]")
 
+"""
 # --- Reflectance and Transmittance (fine) ---
-axs[1].plot((fs_fine - f) / fsr, reflectance, label="Reflectance", color="firebrick")
-axs[1].plot((fs_fine - f) / fsr, transmittance, label="Transmittance", color="green")
-axs[1].set_xlabel("Detuning [FSR]")
+axs[1].plot((fs_fine - f) *1e-6, reflectance, label="Reflectance", color="firebrick")
+axs[1].plot((fs_fine - f)*1e-6, transmittance, label="Transmittance", color="green")
+axs[1].set_xlabel("Frequency around Carrier [MHz]")
 axs[1].set_ylabel("Power Ratio")
 axs[1].legend()
 axs[1].grid()
-
+"""
 plt.tight_layout()
 plt.show()
